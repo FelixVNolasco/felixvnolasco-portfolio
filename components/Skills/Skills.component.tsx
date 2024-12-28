d5ee82c7b32e6cb7099e45fc939c5f62221034d2
felixvnolasco84 <felix@polygonag.com>
2024-12-27 21:43:22 -0600

    :tada:

--- components/Skills/Skills.component.tsx
+++ components/Skills/Skills.component.tsx
@@ -6,8 +6,6 @@ const Skills = () => {
       <p className="text-3xl font-bold text-white">Skills</p>
 
       <div className="text-md my-8 flex flex-col font-medium md:text-xl custom:text-lg">
-
-
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
           <span className="text-white">Typescript</span>
@@ -16,23 +14,22 @@ const Skills = () => {
 
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">ReactJS</span>
+          <span className="text-white">NextJS</span>
           &nbsp;as my Frontend Framework
         </p>
 
-
         <p className="flex flex-row items-center border-b-[0.1px] border-gray-500 py-1 text-slate-300">
           <BsArrowRightShort size="30" />
-          <span className="text-white">.NET Core</span>
-          &nbsp;as my Backend Framework
+          <span className="text-white">Convex, Express, Prisma</span>
+          &nbsp;as my Backend Stack
         </p>
-
       </div>
 
       <p className="text-lg font-medium text-slate-300">
-        ...more skills include <span className="text-white">MongoDB</span>,{" "}
-        <span className="text-white">SQL</span>,{" "}
-        <span className="text-white">Entity Framework</span>{" "}
+        ...more skills include <span className="text-white">Python</span>,{" "}
+        <span className="text-white">ReactJs, VueJs</span>,{" "}
+        <span className="text-white">Shopify (Liquid)</span>,{" "}
+        <span className="text-white">SQL</span>{" "}
       </p>
     </div>
   );
